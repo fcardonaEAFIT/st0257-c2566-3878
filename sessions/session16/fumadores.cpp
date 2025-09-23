0c0366b34b64f08e864a9e606e5ec49349fc8456
Juan Francisco Cardona McCormick (eafit) <fcardona@eafit.edu.co>
2025-09-23 15:14:28 -0500

    Una versión de fumadores funcionando

--- sessions/session16/fumadores.cpp
+++ sessions/session16/fumadores.cpp
@@ -4,11 +4,23 @@
 #include<cstdlib>
 #include<unistd.h>
 
-enum SEMAFOROS { NO_TABACO, NO_PAPEL, NO_CERRILLA, AGENTE };
+enum INGREDIENTE_NO_NECESARIO { NO_TABACO, NO_PAPEL, NO_CERRILLA, AGENTE };
 
+const char *ingredientes_necesarios[] = {
+  "papel y cerilla",
+  "tabaco y cerilla",
+  "tabaco y papel"
+};
+
+const char* ingrediente[] = {
+  "tabaco",
+  "papel",
+  "cerilla"
+};
+		       
 sem_t semaforos[AGENTE + 1];
 
-void* fumador(void *);
+void* fumador(void *fumador_id);
 
 int
 main(int argc, char* argv[]) {
@@ -22,24 +34,36 @@ main(int argc, char* argv[]) {
   srand(time(NULL));
 
   for (int i = 0; i < AGENTE; i++) {
-    pthread_create(&fumadores[i], NULL, fumador, (void *) i);
+    INGREDIENTE_NO_NECESARIO *id = new INGREDIENTE_NO_NECESARIO();
+    *id = (INGREDIENTE_NO_NECESARIO) i;
+    pthread_create(&fumadores[i], NULL, fumador, (void *) id);
   }
   
   while(1) {
     int elemento = random() % 3;
-    sem_post(&semaforos[elemento]);
-    sem_post(&semaforos[AGENTE]);
+
+    std::cout << "Agente poniendo en la mesa: "
+	      << ingredientes_necesarios[elemento]
+	      << std::endl;
+    sleep(random() % 3 + 1);
+    sem_post(&semaforos[elemento]); 
+    sem_wait(&semaforos[AGENTE]);
   }
 
   return 0;
 }
 
-void* fumador(void *arg) {
-  SEMAFOROS id = (SEMAFOROS) arg;
+void* fumador(void *fumador_id) {
+  INGREDIENTE_NO_NECESARIO *id = (INGREDIENTE_NO_NECESARIO*) fumador_id;
   for(;;) {
-    sem_wait(&semaforos[id]);
-    std::cout << "Soy el fumador: " << id << " Estoy fumando" << endl;
-    sleep(random() % 5 + 1);
+    
+    sem_wait(&semaforos[*id]);
+    std::cout << "Fumador con "
+	      << ingrediente[*id]
+	      << " recibio "
+	      << ingredientes_necesarios[*id]
+	      << " los enrollo y está fumando." << std::endl;
+    sleep(random() % 6 + 1);
     sem_post(&semaforos[AGENTE]);
   }
 }
