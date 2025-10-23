1bf9c662ddf9e3a05ea49956e019896294ae53c8
Juan Francisco Cardona McCormick (eafit) <fcardona@eafit.edu.co>
2025-10-23 14:12:27 -0500

    Adding .gitignore and direcction has added a function to keep DRY scheme

--- sessions/session23/direccion.c
+++ sessions/session23/direccion.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
+void
+imprimir(unsigned int npag,
+	 unsigned int desp);
+
 int
 main(int argc, char *argv) {
   unsigned int direccion = 0x49176189;
   unsigned int m = 32;
   unsigned int n = 9;
-  unsigned int diff = m -n;
+  unsigned int diff = m - n;
 
   unsigned int desp = (direccion << diff) >> diff;
-  unsigned int npag = (direccion >> n) ;
+  unsigned int npag = (direccion >> n);
 
-  printf("nro pagina: %x desplazamiento: %x\n", npag, desp);
+  imprimir(npag, desp);
 
   desp = direccion % 512;
   npag = direccion / 512;
 
-  printf("nro pagina: %x desplazamiento: %x\n", npag, desp);
+  imprimir(npag, desp);
 
   unsigned int ALL_ONES = ~((unsigned int) 0);
   unsigned int MASCARA_NROPAGINA = (ALL_ONES >> n) << n;
@@ -24,7 +28,14 @@ main(int argc, char *argv) {
   desp = direccion & MASCARA_DESP;
   npag = (direccion & MASCARA_NROPAGINA) >> n;
 
-  printf("nro pagina: %x desplazamiento: %x\n", npag, desp);
+  imprimir(npag, desp);
 
   return 0;
+}
+
+void
+imprimir(unsigned int npag,
+	 unsigned int desp) {
+  
+  printf("nro pagina: 0x%X desplazamiento: 0%X\n", npag, desp);
 }
